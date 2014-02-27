fdbb7c130b7ffa52158ee86fb6202c8010505468
Kyle Chadha <kyle.chadha@gmail.com>
2014-02-27 14:16:43 -0800

    Integrate geokit to search nearby stops

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,2 +1,13 @@
 class StopsController < ApplicationController
+
+  def index
+    @stops = Stop.all
+    @stopInfo = Stop.within(0.2, :origin => [params[:lat], params[:lon]])
+
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
 end
--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,2 +1,6 @@
 class Stop < ActiveRecord::Base
+
+  acts_as_mappable :lat_column_name => :lat,
+                   :lng_column_name => :lon
+
 end
