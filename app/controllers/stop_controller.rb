1137fe6513352277e6b8d7c7876dddb2b42ad712
Kate Bankert <kbankert@gmail.com>
2014-02-27 12:31:43 -0800

    add spec gem and ran four tests - presence and numericality for Stop.lat and Stop.long

---
+++ app/controllers/stop_controller.rb
@@ -0,0 +1,12 @@
+# class StopController < ApplicationController
+
+#   def new
+#   end
+
+#   def create
+#     @stop = Stop.new(params.require(:stop).permit(:gtfs_stop_id, :code, :name, :desc, :lat, :lon, :zone_id, :url, :location_type, :parent_station))
+
+#   end
+
+
+# end
--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,2 +1,6 @@
 class Stop < ActiveRecord::Base
+
+  validates :lat, :lon, presence:true
+  validates :lat, :lon, numericality:true
+
 end
