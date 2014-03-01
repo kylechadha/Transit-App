c1ddd152765c3ffcd0ad3c84bb64bebc88157f68
Kyle Chadha <kyle.chadha@gmail.com>
2014-02-28 17:28:58 -0800

    Partway complete routes headsign calcs

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -2,7 +2,9 @@ class StopsController < ApplicationController
 
   def index
     @stops = Stop.all
-    @nearby = Stop.by_distance(:origin => [params[:lat], params[:lon]]).limit(10)
+    @nearby = Stop.by_distance(:origin => [params[:lat], params[:lon]]).limit(12)
+    # For loading up console: Stop.by_distance(:origin => [34.013072799999996, -118.4950833]).limit(12)
+
 
     respond_to do |format|
       format.html
