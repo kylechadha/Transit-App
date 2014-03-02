53b7998221ce02d1e223ac5a790dd69b162288ac
Kyle Chadha <kyle.chadha@gmail.com>
2014-03-02 12:21:31 -0800

    Seed route direction headsigns

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,11 +17,38 @@
 #   end
 # end
 
-# Route.all.each do |route|
-#   agency = Agency.find_by(gtfs_agency_id: route.gtfs_agency_id)
-#   route.agency_id = agency.id
-#   route.save
-# end
+# After exploring the possibility of calculting Route "Direction Headsigns" from the Trips model, incomplete data requires that we manually input this information
+
+headsigns = {
+  "1944" => { 0 => "To UCLA", 1 => "To Venice" },
+  "1945" => { 0 => "To UCLA", 1 => "To Santa Monica" },
+  "1946" => { 0 => "To Santa Monica / UCLA", 1 => "To Green Line Station" },
+  "1947" => { 0 => "To Westside Pavilion", 1 => "To Downtown Santa Monica" },
+  "1948" => { 0 => "To Expo Culver City Station", 1 => "Santa Monica" },
+  "1949" => { 0 => "To Palms", 1 => "To Santa Monica College" },
+  "1950" => { 0 => "To Rimpau Transit Center", 1 => "To Santa Monica" },
+  "1951" => { 0 => "To UCLA", 1 => "To Santa Monica" },
+  "1952" => { 0 => "To Pacific Palisades", 1 => "To Santa Monica" },
+  "1953" => { 0 => "To Downtown Los Angeles", 1 => "To Santa Monica" },
+  "1955" => { 0 => "To Expo Culver City Station", 1 => "To UCLA" },
+  "1956" => { 0 => "To Rimpau Transit Center", 1 => "To Westside Pavilion" },
+  "1957" => { 0 => "To Brentwood", 1 => "To Culver City" },
+  "1958" => { 0 => "To Expo Culver City Station", 1 => "To Santa Monica" },
+  "1959" => { 0 => "To Expo Culver City Station", 1 => "To UCLA" },
+  "1960" => { 0 => "To Santa Monica", 1 => "To LAX / Green Line Station" },
+  "1961" => { 0 => "To Metro Purple Station", 1 => "To Santa Monica" },
+  "1962" => { 0 => "Crosstown Ride" },
+  "1963" => { 0 => "Sunset Ride" }
+}
+
+Route.all.each do |route|
+  # agency = Agency.find_by(gtfs_agency_id: route.gtfs_agency_id)
+  # route.agency_id = agency.id
+
+  route.outbound = headsigns[route.gtfs_route_id][1]
+  route.inbound = headsigns[route.gtfs_route_id][0]
+  route.save
+end
 
 # # Created shape ID as " " since there was an extra space in the file! >.<
 # Trip.delete_all
