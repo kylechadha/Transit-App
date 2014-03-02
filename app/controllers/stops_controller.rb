b6a94f5f6810e97b7142adbe639b20ca4b4288f6
Kyle Chadha <kyle.chadha@gmail.com>
2014-03-02 13:59:29 -0800

    Add end stop view and controller action in Stops

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -2,12 +2,16 @@ class StopsController < ApplicationController
 
   def index
     @stops = Stop.all
-    @nearby = Stop.by_distance(:origin => [params[:lat], params[:lon]]).limit(200)
+    @nearby = Stop.by_distance(:origin => [params[:lat], params[:lon]]).limit(16)
     # For loading up console: Stop.by_distance(:origin => [34.013072799999996, -118.4950833]).limit(12)
 
     respond_to do |format|
       format.html
       format.js
     end
+  end
+
+  def end
+    @route = Route.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 TransitAppThree::Application.routes.draw do
 
+  get 'stops/end', :to => "stops#end"
+
   resources :welcome, only: [:index]
   resources :users, only: [:index, :show, :create, :new]
   resources :stops, only: [:index, :show, :edit, :update, :destroy]
