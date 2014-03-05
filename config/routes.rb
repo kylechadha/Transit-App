1cddcec209e472c351560e110626ef0efc21be5b
Kate Bankert <kbankert@gmail.com>
2014-03-04 21:23:28 -0800

    pasted geolocation tracker JS in journeys view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 TransitAppThree::Application.routes.draw do
 
+  get "journeys/index"
+  get "journeys/show"
+  get "journeys/new"
+  get "journeys/create"
   get 'stops/destination/:id', :to => "stops#destination", as: "stops_destination"
 
   resources :welcome, only: [:index]
