e37dd418ac7c325e47bf3d9b8cbe90cf63a95dc6
Kyle Chadha <kyle.chadha@gmail.com>
2014-03-05 19:06:36 -0800

    Add preferences view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 TransitAppThree::Application.routes.draw do
 
-  get 'journeys/destination/:id', :to => "journeys#destination", as: "journeys_destination"
   get 'journeys/selection', :to => "journeys#selection", as: "journeys_selection"
+  get 'journeys/destination/:id', :to => "journeys#destination", as: "journeys_destination"
+  get 'journeys/preferences', :to => "journeys#preferences", as: "journeys_preferences"
 
   resources :welcome, only: [:index]
   resources :users, only: [:index, :show, :create, :new]
